502b006823232abb686787bdf33d2c78c6c6de46
Diego Muracciole <diegomuracciole@gmail.com>
2018-05-18 16:14:11 -0300

    Setup notifications on Android & icons fix

--- android/app/src/main/java/com/orbitingapp/MainApplication.java
+++ android/app/src/main/java/com/orbitingapp/MainApplication.java
@@ -17,11 +17,11 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
-        @Override
-        protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
-        }
-    
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -31,8 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
+          new ReactNativePushNotificationPackage(),
+          new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
       );
     }
 
