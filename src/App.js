ee1d8eab513319ba648eec6f380cf20e24187e74
Diego Muracciole <diegomuracciole@gmail.com>
2018-05-18 19:07:08 -0300

    Add webview that shows login page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,37 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, AppState } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { compose } from 'recompose';
+import WebView from './components/WebView';
 import codePush from './services/codePush';
 import pushNotifications from './services/pushNotifications';
 
+const LOGIN_URL = 'https://www.republik.ch/anmelden';
+
 class App extends Component {
   render() {
-    let progressView;
-
-    if (this.props.progress) {
-      progressView = (
-        <Text style={styles.messages}>
-          {this.props.progress.receivedBytes} of {this.props.progress.totalBytes} bytes received
-        </Text>
-      );
-    }
-
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.props.sync}>
-          <Text style={styles.syncButton}>Press for background sync</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.props.syncImmediate}>
-          <Text style={styles.syncButton}>Press for dialog-driven sync</Text>
-        </TouchableOpacity>
-        {progressView}
-        <TouchableOpacity onPress={this.props.getUpdateMetadata}>
-          <Text style={styles.syncButton}>Press for Update Metadata</Text>
-        </TouchableOpacity>
-        <Text style={styles.messages}>{this.props.syncMessage || ""}</Text>
-      </View>
+      <WebView
+        automaticallyAdjustContentInsets={false}
+        style={styles.webView}
+        source={{uri: LOGIN_URL}}
+        javaScriptEnabled
+        startInLoadingState
+        scalesPageToFit
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-    paddingTop: 50
+    backgroundColor: '#F5FCFF',
   },
-  messages: {
-    marginTop: 30,
-    textAlign: "center",
-  },
-  syncButton: {
-    color: "red",
-    fontSize: 17
+  webView: {
+    flex: 1,
+    marginTop: 20,
   }
 });
-
 
 export default compose(
   codePush,
