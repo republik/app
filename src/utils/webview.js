6b55d554849ce0187e9167f46aa4aef9d8b94099
Diego Muracciole <diegomuracciole@gmail.com>
2018-06-25 19:30:47 +0200

    Add pull to refresh webview (#54)

---
+++ src/utils/webview.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-undef */
+
+export const injectedJavaScriptImpl = function () {
+  // Navigation polyfills
+  // Injected code to spy on browser's navigation history
+  // Native WebView onNavigationStateChange does not recognize SPA page transitions,
+  // so we inject code that enables to spy on changes, sending messages.
+
+  var pushState = window.history.pushState
+  var replaceState = window.history.replaceState
+  var back = window.history.back
+
+  function updateNavState (url, canGoBack) {
+    var hash = location.hash !== '' ? '?' + location.hash : ''
+    var prefix = location.protocol + '//' + location.host
+
+    window.postMessage(JSON.stringify({
+      type: 'navigation',
+      url: url ? (prefix + url) : (prefix + location.pathname + hash),
+      canGoBack
+    }))
+  };
+
+  window.history.pushState = function () {
+    updateNavState(arguments[2], true)
+    return pushState.apply(window.history, arguments)
+  }
+
+  window.history.replaceState = function () {
+    updateNavState(arguments[2])
+    return replaceState.apply(window.history, arguments)
+  }
+
+  window.history.back = function () {
+    updateNavState()
+    return back.apply(window.history)
+  }
+
+  window.onload = function () {
+    updateNavState()
+  }
+
+  window.onpopstate = function () {
+    updateNavState()
+  }
+
+  window.onhashchange = function () {
+    updateNavState()
+  }
+
+  // Scrolling polyfills
+
+  function debounce (func, wait) {
+    var timeout
+
+    return function () {
+      var context = this
+      var args = arguments
+      var later = function () {
+        timeout = null
+        func.apply(context, args)
+      }
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+    }
+  }
+
+  document.addEventListener('message', function (event) {
+    var message = JSON.parse(event.data)
+
+    if (message.type === 'scroll-to-top') {
+      window.scrollTo(0, 0)
+    }
+  })
+
+  var onScroll = debounce(function (scroll) {
+    window.postMessage(JSON.stringify({
+      type: 'scroll',
+      payload: { x: window.scrollX, y: window.scrollY }
+    }))
+  }, 150)
+
+  window.addEventListener('scroll', onScroll)
+}
+
+// Implementation IIFE ready to inject
+export const injectedJavaScript = `(${injectedJavaScriptImpl.toString()})()`
