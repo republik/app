9360ffa35be4249aac7a5e459f406b663add7a5d
Diego Muracciole <diegomuracciole@gmail.com>
2018-07-19 11:32:27 -0300

    App notifications (#95)

---
+++ src/screens/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { StyleSheet, Image, View } from 'react-native'
+import { compose } from 'react-apollo'
+import WebView from '../components/WebView'
+import navigator from '../services/navigation'
+import Logo from '../assets/images/logo-title.png'
+import { parseURL, handleEnv } from '../utils/url'
+
+const LoginHeader = () => (
+  <View style={styles.headerContainer}>
+    <Image source={Logo} style={styles.logo} />
+  </View>
+)
+
+class Login extends Component {
+  onMessage = message => {
+    switch (message.type) {
+      case 'close-auth-overlay':
+        return navigator.goBack()
+    }
+  }
+
+  onNavigationStateChange = (data) => {
+    const url = parseURL(data.url)
+    // close overlay instead of going elsewhere
+    if (url.path !== '/mitteilung') {
+      navigator.goBack()
+      return false
+    }
+    return true
+  }
+
+  render () {
+    const uri = handleEnv(this.props.navigation.getParam('url'))
+
+    return (
+      <WebView
+        source={{ uri }}
+        onMessage={this.onMessage}
+        style={styles.container}
+        onNavigationStateChange={this.onNavigationStateChange}
+        ref={node => { this.webview = node }}
+      />
+    )
+  }
+}
+
+Login.navigationOptions = ({ screenProps }) => ({
+  headerTitle: <LoginHeader />,
+  headerRight: <View />,
+  headerStyle: { backgroundColor: '#FFFFFF' }
+})
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    zIndex: 100
+  },
+  headerContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 150,
+    height: 25
+  }
+})
+
+export default compose(
+)(Login)
