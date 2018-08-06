f62b46f5f0824e5435dc1878c4f766fb4d2548de
Diego Muracciole <diegomuracciole@gmail.com>
2018-08-06 16:21:32 -0300

    Add clear bundle toggle setting to ios

--- ios/orbitingapp/AppDelegate.m
+++ ios/orbitingapp/AppDelegate.m
@@ -28,6 +28,7 @@
   NSString *graphQLUrl = [[NSUserDefaults standardUserDefaults] stringForKey:@"graphql_url"];
   NSString *wsUrl = [[NSUserDefaults standardUserDefaults] stringForKey:@"ws_url"];
   NSString *assetsUrl = [[NSUserDefaults standardUserDefaults] stringForKey:@"assets_url"];
+  Boolean clearOTABundle = [[NSUserDefaults standardUserDefaults] boolForKey:@"clear_ota"];
 
   [[NSUserDefaults standardUserDefaults] setObject:appVersion forKey:@"version_preference"];
   [[NSUserDefaults standardUserDefaults] setObject:buildNumber forKey:@"build_preference"];
@@ -47,6 +48,11 @@
 
   if ([assetsUrl length] == 0) {
     [[NSUserDefaults standardUserDefaults] setObject:[env objectForKey:@"ASSETS_SERVER_BASE_URL"] forKey:@"assets_url"];
+  }
+  
+  if (clearOTABundle) {
+    [OTA clearBundle];
+    [[NSUserDefaults standardUserDefaults] setObject:false forKey:@"clear_ota"];
   }
 }
 
