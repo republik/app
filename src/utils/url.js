7c1c76acbb6c534814d2429df4879f424b1366e0
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-08-09 18:17:51 +0200

    prevent dev crash when urls are missing

--- src/utils/url.js
+++ src/utils/url.js
@@ -51,7 +51,7 @@ export const parseURL = value => {
 // localhost does not work on Android.
 // https://stackoverflow.com/questions/4336394/webview-and-localhost
 export const handleEnv = value => {
-  if (Config.ENV === 'development') {
+  if (Config.ENV === 'development' && value) {
     return Platform.select({
       ios: value,
       android: value.replace('localhost', '10.0.2.2')
