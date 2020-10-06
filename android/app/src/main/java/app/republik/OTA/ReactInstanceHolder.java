ad05a226d464584b27f6d5de0b987d9a1410996f
Olivier Baumann <olivier@iovi.io>
2020-10-06 15:29:05 +0200

    new RN 0.62.2 app

--- android/app/src/main/java/app/republik/OTA/ReactInstanceHolder.java
+++
@@ -1,7 +0,0 @@
-package app.republik.OTA;
-
-import com.facebook.react.ReactInstanceManager;
-
-public interface ReactInstanceHolder {
-    ReactInstanceManager getReactInstanceManager();
-}
--- ios/dummy.swift
+++
@@ -1,9 +0,0 @@
-//
-//  dummy.swift
-//  orbitingapp
-//
-//  Created by Diego Muracciole on 7/6/18.
-//  Copyright © 2018 Facebook. All rights reserved.
-//
-
-import Foundation
