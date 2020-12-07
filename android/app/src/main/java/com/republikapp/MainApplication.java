c00ca38ffe9b164e1011c75b9017c877c52a46c6
Olivier Baumann <olivier@iovi.io>
2020-12-07 17:53:42 +0100

    android compiles

--- android/app/src/main/java/com/republikapp/MainApplication.java
+++ android/app/src/main/java/com/republikapp/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new SplashScreenReactPackage());
+          packages.add(new TrackPlayer());
           return packages;
         }
 
