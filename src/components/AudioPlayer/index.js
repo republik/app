c8c547c9f319b519a8394c5e7d3644b843477bfa
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-01-29 13:27:10 +0100

    rm shadow

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -145,13 +145,6 @@ const AudioPlayer = () => {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 4.65,
     elevation: 7,
   },
   player: {
