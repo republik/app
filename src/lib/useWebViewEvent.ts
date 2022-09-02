69d421691a77fea3977e882b17aeb2afc33fbae9
Tobias Maier <trm217@protonmail.com>
2022-09-02 10:08:52 +0200

    feat: add useWebViewEvent-hook to handle registering and removing event-handlers

---
+++ src/lib/useWebViewEvent.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import WebViewEventEmitter from './WebViewEventEmitter';
+
+type EventHandler<E> = (eventData: E) => void;
+
+/**
+ * useWebViewEvent allows to subscribe to events emitted by the web-ui.
+ * @param eventName The name of the event to subscribe to.
+ * @param handler The handler to call when the event is emitted.
+ */
+function useWebViewEvent<E = Event>(eventName: string, handler: EventHandler<E>) {
+  useEffect(() => {
+    WebViewEventEmitter.addListener(eventName, handler)
+    return () => {
+        WebViewEventEmitter.removeListener(eventName, handler)
+    }
+  }, [eventName, handler])
+}
+
+export default useWebViewEvent
